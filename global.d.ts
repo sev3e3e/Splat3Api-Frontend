831aa8488c48407a325f7a5cbaa34302a24966ac
sev3e3e <fc2tokaqq+sev3e3e@gmail.com>
2023-06-21 10:51:43 +0900

    delete unused files

--- global.d.ts
+++ global.d.ts
@@ -1,2 +1,12 @@
+import "@tanstack/react-table";
+
 declare module "*.module.css";
 declare module "*.module.scss";
+
+declare module "@tanstack/table-core" {
+    interface ColumnMeta<TData extends RowData, TValue> {
+        width?: string;
+        maxWidth?: string;
+        minWidth?: string;
+    }
+}
--- src/_pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from "next/app";
-import "../styles/pages/_app.css";
-
-function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
-}
-
-export default App;

d815555a9ddc73ba19c489cef0810212d3627e98
sev3e3e <fc2tokaqq+sev3e3e@gmail.com>
2023-08-01 13:40:55 +0900

    remove hard coding

--- src/app/config.ts
+++
@@ -1,4 +0,0 @@
-export const SITE_URL =
-    process.env.NODE_ENV !== "production"
-        ? "http://127.0.0.1:3000"
-        : "http://127.0.0.1:3000";
--- src/components/table/playerTable.tsx
+++ src/components/table/playerTable.tsx
@@ -2,6 +2,7 @@
 
 import useSWRImmutable from "swr/immutable";
 import _table from "./innerTable";
+import router from "next/router";
 
 export const XRankPlayerTable = ({
     formattedDate,
@@ -12,22 +13,10 @@ export const XRankPlayerTable = ({
     mode: string;
     region: "atlantic" | "pacific";
 }) => {
-    // get from external source
     const { data, error } = useSWRImmutable(
         [formattedDate, mode, region],
         ([formattedDate, mode, region]) => fetcher(formattedDate, mode, region)
     );
-
-    console.log(
-        `http://localhost:3000/api/xrank/${formattedDate}/${mode}/${region}`
-    );
-
-    console.log(data);
-    // const data = await xRankingFetcher(
-    //     formattedDate,
-    //     mode,
-    //     region.charAt(0) as "a" | "p"
-    // );
 
     if (!data) {
         return <>loading!!!!!!!!!!!</>;
@@ -47,25 +36,12 @@ export const XRankPlayerTable = ({
                 ]?.edges!
             }
         />
-        // <>
-        //     {JSON.stringify(
-        //         data?.data.node[
-        //             mode === "clamblitz"
-        //                 ? "xRankingCl"
-        //                 : mode === "rainmaker"
-        //                 ? "xRankingGl"
-        //                 : mode === "splatzones"
-        //                 ? "xRankingAr"
-        //                 : "xRankingLf"
-        //         ]?.edges!
-        //     )}
-        // </>
     );
 };
 
 async function fetcher(formattedDate: string, mode: string, region: string) {
     return fetch(
-        `http://localhost:3000/api/xrank/${formattedDate}/${mode}/${region}`
+        `${process.env.NEXT_PUBLIC_SITE_URL}/api/xrank/${formattedDate}/${mode}/${region}`
     ).then((r) => r.json());
 }
 
